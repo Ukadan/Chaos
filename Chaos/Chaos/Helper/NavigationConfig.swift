414d394deea3fdc81f45c3dadb83693d46243aaa
a.ukadan <a.ukadan@arbuz.kz>
2023-08-15 06:28:09 +0500

    future navigation Config

---
+++ Chaos/Chaos/Helper/NavigationConfig.swift
@@ -0,0 +1,21 @@
+//
+//  NavigationConfig.swift
+//  Chaos
+//
+//  Created by Ali on 13.08.2023.
+//
+
+import SwiftUI
+
+//struct NavigationConfigurator: UIViewControllerRepresentable {
+//    var configure: (UINavigationController) -> Void = { _ in }
+//
+//    func makeUIViewController(context: UIViewControllerRepresentableContext<NavigationConfigurator>) -> UIViewController {
+//        UIViewController()
+//    }
+//    func updateUIViewController(_ uiViewController: UIViewController, context: UIViewControllerRepresentableContext<NavigationConfigurator>) {
+//        if let nc = uiViewController.navigationController {
+//            self.configure(nc)
+//        }
+//    }
+//}
